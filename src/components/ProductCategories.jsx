c29c73791e5f8c1a02cf99ea071d9c6d00004445
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2024-12-21 14:55:26 +0500

    done

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
+import mainDish from '../assets/pngwing 4.png';
+import breakfast from '../assets/pngwing 6.png';
+import dessert from '../assets/pngwing 3.png';
+import All from '../assets/pngwing 7.png';
+const ProductCategories = () => {
+    const categories = [
+        { name: 'Main dish', imgSrc: mainDish},
+        { name: 'Breakfast', imgSrc: breakfast },
+        { name: 'Dessert', imgSrc: dessert },
+        { name: 'Browse All', imgSrc: All },
 
- const ProductCategories = () => {
-    const categoryCards = ()=>{
-        return(
+    ];
+
+    const categoryCards = () => {
+        return (
             <div className="product-category-cards">
-                <div className="product-category-card">
-                    <img src="https://images.unsplash.com/photo-1612833463041-1b5e6d7c3b6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
-                    <h5>Breakfast</h5>
+                {categories.map((category, index) => (
+                    <div className="product-category-card" key={index}>
+                        <img src={category.imgSrc} alt={category.name} />
+                        <h5>{category.name}</h5>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    return (
+        <>
+            <div className="product-category-section">
+                <div className="product-category-top">
+                    <div className="product-category-subtitle">
+                        <h6>Customer Favorites</h6>
+                    </div>
+                    <div className="product-category-subtitle">
+                        <h2>Popular Categories</h2>
+                    </div>
+                    <div className="product-category-cards d-flex justify-content-between">
+                        {categoryCards()}
+                    </div>
                 </div>
             </div>
-        )
-    }
-  return (
-    <>
-<div className="product-category-section">
-    <div className="product-category-top">
-        <div className="product-category-subtitle">
-            <h6>Customer Favorites</h6>
-        </div>
-        <div className="product-category-subtitle">
-            <h2>Popular Catagories</h2>
-        </div>
-      
-        <div className="product-category-cards">
-        {
-          categoryCards()        
-        }
-        </div>
-    </div>
-</div>
-    </>
-  )
-}
+        </>
+    );
+};
+
 export default ProductCategories;
