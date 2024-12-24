79b5892a95e10d403b5e03ace33f36cac563f497
Nazish-Kiran <nazishkirancosmos123@gmail.com>
2024-12-24 13:13:57 +0500

    done

--- src/components/ProductCategories.jsx
+++ src/components/ProductCategories.jsx
@@ -5,20 +5,23 @@ import dessert from '../assets/pngwing 3.png';
 import All from '../assets/pngwing 7.png';
 const ProductCategories = () => {
     const categories = [
-        { name: 'Main dish', imgSrc: mainDish},
-        { name: 'Breakfast', imgSrc: breakfast },
-        { name: 'Dessert', imgSrc: dessert },
-        { name: 'Browse All', imgSrc: All },
+        { name: 'Main dish', imgSrc: mainDish, quantity: "(86 dishes)" },
+        { name: 'Breakfast', imgSrc: breakfast, quantity: "(12 break fast)" },
+        { name: 'Dessert', imgSrc: dessert, quantity: "(48 dessert)" },
+        { name: 'Browse All', imgSrc: All, quantity: "(255 Items)" },
 
     ];
 
     const categoryCards = () => {
         return (
-            <div className="product-category-cards">
+            <div className="product-category-cards d-flex align-items-center justify-content-center w-100 gap-4 pt-3 pt-md-5 pb-5">  
                 {categories.map((category, index) => (
-                    <div className="product-category-card" key={index}>
+                    <div className="product-category-card d-flex flex-column align-items-center" key={index}>
+                        <div className="product-category-figure-wrapper rounded-circle d-flex justify-content-center align-items-center">
                         <img src={category.imgSrc} alt={category.name} />
+                        </div>
                         <h5>{category.name}</h5>
+                        <p>{category.quantity}</p>
                     </div>
                 ))}
             </div>
@@ -27,7 +30,7 @@ const ProductCategories = () => {
 
     return (
         <>
-            <div className="product-category-section">
+            <div className="product-category-section text-center">
                 <div className="product-category-top">
                     <div className="product-category-subtitle">
                         <h6>Customer Favorites</h6>
